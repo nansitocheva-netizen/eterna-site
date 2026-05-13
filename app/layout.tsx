62420885c05649c45dc1c09555efda9ca40b5a7c
Nansi Tocheva <nansitocheva@gmail.com>
2026-05-13 13:11:38 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,13 @@ export default function RootLayout({
       lang="en"
         className={`${geistSans.variable} ${geistMono.variable}`}
     >
+       <head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
+    </head>
+    
       <body
   style={{
     width: "100%",
